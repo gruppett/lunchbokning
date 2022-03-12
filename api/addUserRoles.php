82336574673a28f727f61a905f981f9457e4af4f
Kim Ståhl <flaxarn85@gmail.com>
2022-03-12 13:27:02 +0200

    ge grundroll om ingen roll matchar

--- api/addUserRoles.php
+++ api/addUserRoles.php
@@ -35,7 +35,7 @@ if (isset($_POST['mail'])) {
     } 
 }
 
-//* Kolla om metod är POST med roles och i array
+//* Kolla om metod är POST med roles
 if (!isset($_POST['roles']))  {
     $error[] = "Bad indata. Roles saknas";
 }
@@ -146,6 +146,19 @@ if (count($roller) >= 1) {
             exit();
         }
     }
+} else {
+    //* om ingen roll är rätt ge employee Personal roll
+    $sql = $db->prepare("INSERT INTO employee_roles (EmployeeID, RoleID) VALUES ('$nyID', '5')");
+    if  ($sql->execute()){
+        $rec = ["Spara lyckades för $nyID med grundrollen Personal"];
+        $out->roles[] = $rec;
+    } else {
+        //! Meddela fel
+        $fel = $db->error;
+        $out->error = ["Fel vid spara", " $fel"];
+        echo skickaJSON($out, 400);
+        exit();
+    } 
 }
 
 //* Skicka tillbaka json med svar om ok spara och avsluta
