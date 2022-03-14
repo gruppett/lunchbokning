72b4bc0b7b2dbed6806ecb86348322fcfc52cf10
October45 <vikto45.t@gmail.com>
2022-03-14 13:44:32 +0200

    Popup on cursor position

--- src/components/Views/Calendar/overview_popup.tsx
+++ src/components/Views/Calendar/overview_popup.tsx
@@ -4,7 +4,8 @@ function Overview_popup(pos: any) {
   console.log(pos);
   return (
     <div
-      className={"bg-red-500 absolute left-[" + pos.x + "] top-[" + pos.y + "]"}
+      className={"bg-red-500 absolute p-1 hover:"}
+      style={{ left: pos.x, top: pos.y }}
     >
       <button>Click Me!</button>
     </div>
