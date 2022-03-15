d96237e2c0bd8374c3f8f343e5fcd8fdf758ecdf
October45 <vikto45.t@gmail.com>
2022-03-15 11:13:41 +0200

    unMount popup on !click

--- src/components/Views/Quick_overview.tsx
+++ src/components/Views/Quick_overview.tsx
@@ -1,27 +1,30 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { GraphContext } from "../App/App";
 import "./Calendar/popup";
 import Popup from "./Calendar/popup";
-import ReactDOM from "react-dom";
+import ReactDOM, { unmountComponentAtNode } from "react-dom";
 
 function Overview() {
   const [value, onChange] = useState(new Date());
   const { user } = useContext(GraphContext);
 
   return (
-    <main className="h-auto m-3" id="main">
+    <main className="h-full m-3" id="main">
       <Calendar
         onChange={onChange}
         value={value}
-        className="!w-full !h-full"
-        minDetail="year"
+        className="!w-full !h-full z-10 relative"
+        minDetail="month"
         onClickDay={(value, event) =>
           ReactDOM.render(
             <Popup x={event.clientX} y={event.clientY} />,
             document.getElementById("popup")
           )
+        }
+        onActiveStartDateChange={() =>
+          unmountComponentAtNode(document.getElementById("popup") as Element)
         }
         tileContent={({ date, view }) =>
           view === "month" && date.getDay() === 2 ? (
