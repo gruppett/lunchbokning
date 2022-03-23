8964b4ac1e80991ee33f24791c9aeb82adfeca26
Jann Tötterman <jann.totterman@gmail.com>
2022-03-23 11:24:59 +0200

    change to mail based

--- src/pages/settings/SettingsGroups.tsx
+++ src/pages/settings/SettingsGroups.tsx
@@ -1,51 +1,51 @@
 import React, {useState} from 'react'
+import { mailToName } from '../../helpers/mailToName'
 
 const dummyGroupData = [
   {
     name: "IT32",
     count: 16,
-    primaryHandler: "Jimmy Jansson",
+    primaryHandler: "Jimmy.Jansson@gymnasium.ax",
     handlers: [
-      "Jimmy Jansson"
+      "Jimmy.Jansson@gymnasium.ax"
     ]
   },
   {
     name: "SERV32",
     count: 16,
-    primaryHandler: "Simmy Sansson",
+    primaryHandler: "Simmy.Sansson@gymnasium.ax",
     handlers: [
-      "Simmy Sansson",
-      "Mimmy Mansson"
+      "Simmy.Sansson@gymnasium.ax",
+      "Mimmy.Mansson@gymnasium.ax"
     ]
   },
   {
     name: "MERK32",
     count: 16,
-    primaryHandler: "Mimmy Mansson",
+    primaryHandler: "Mimmy.Mansson@gymnasium.ax",
     handlers: [
-      "Mimmy Mansson",
-      "Simmy Sansson",
-      "Iimmy Iansson",
-      "Kimmy Kansson",
+      "Mimmy.Mansson@gymnasium.ax",
+      "Simmy.Sansson@gymnasium.ax",
+      "Iimmy.Iansson@gymnasium.ax",
+      "Kimmy.Kansson@gymnasium.ax",
     ]
   }
 ]
 
 const dummyHandlerData = [
   {
-    name: "Jimmy Jansson",
+    name: "Jimmy.Jansson@gymnasium.ax",
     id: 0
   },
   {
-    name: "Simmy Sansson",
+    name: "Simmy.Sansson@gymnasium.ax",
     id: 1
   },
   {
-    name: "Mimmy Mansson",
+    name: "Mimmy.Mansson@gymnasium.ax",
     id: 2
   },
 ]
-
 
 
 
@@ -61,6 +61,8 @@ function SettingsGroups() {
   return (
     <div className='flex gap-3 flex-col p-3 bg-slate-50 sm:w-max'>
     <div className="flex gap-3 items-start sm:flex-wrap flex-col sm:flex-row">
+      <div>
+        <h2>Grupper</h2>
       <table className='table-auto text-left border-collapse bg-white'>
         <thead>
           <tr>
@@ -74,11 +76,13 @@ function SettingsGroups() {
           <tr key={i} onClick={() => selectGroup(i)} className='cursor-pointer hover:bg-slate-100 even:bg-slate-50'>
             <td className='border p-1'>{d.name}</td>
             <td className='border p-1'>{d.count}</td>
-            <td className='border p-1'>{d.primaryHandler}</td>
+            <td className='border p-1'>{mailToName(d.primaryHandler)}</td>
           </tr> 
         ))}
         </tbody>
       </table>
+      
+        </div>
       <div className='flex flex-col gap-1'>
         <h2>Lägg till / Ändra</h2>
         <label htmlFor="groupName">Namn</label>
@@ -89,7 +93,7 @@ function SettingsGroups() {
         <select name="groupHandler" id="groupHandler" className='bg-white p-1'>
           <option value="">Välj primär handledare</option>
           {dummyHandlerData.map((d, i) => (
-            <option value={d.id} key={i}>{d.name}</option>
+            <option value={d.id} key={i}>{mailToName(d.name)}</option>
           ))}
         </select>
         <button className='px-3 py-1 w-min bg-blue-300'>Spara</button>
@@ -106,7 +110,7 @@ function SettingsGroups() {
           <tbody>
             {dummyGroupData[selectedGroup].handlers.map((d, i) => (
               <tr key={i} className='bg-white even:bg-slate-50'>
-                <td className={`border p-1 ${dummyGroupData[selectedGroup].primaryHandler === d ? " font-bold" : ""}`} >{d}</td>
+                <td className={`border p-1 ${dummyGroupData[selectedGroup].primaryHandler === d ? " font-bold" : ""}`} >{mailToName(d)}</td>
                 {dummyGroupData[selectedGroup].primaryHandler !== d
                 ?
                 <td className="border p-1">
@@ -122,7 +126,7 @@ function SettingsGroups() {
           <select id='handlerName' name='handerName' className="bg-white p-1">
             <option value="">Välj handledare</option>
               {dummyHandlerData.map((d, i) => (
-                <option key={i} value={d.name}>{d.name}</option>
+                <option key={i} value={d.name}>{mailToName(d.name)}</option>
               ))}
           </select>
           <button className='px-3 py-1 w-min whitespace-nowrap bg-blue-300'>Lägg till</button>
