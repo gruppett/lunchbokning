f64c682d292dfe435e0dae2a00ffec6c035c50f1
Jann Tötterman <jann.totterman@gmail.com>
2022-03-24 08:59:18 +0200

    auth

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,6 +6,7 @@ import Page from "../Page/Page";
 import { BrowserRouter } from "react-router-dom";
 import Spinner from "../Spinner/Spinner";
 import SignIn from "../SignIn/SignIn";
+import SignOutButton from "../SignOutButton/SignOutButton";
 
 interface GraphContextInterface {
   user: UserInterface;
@@ -26,6 +27,8 @@ interface UserInterface {
   userPrincipalName: string;
 }
 
+const allowedGroups = ["M365-DAT19Projektgrupp1"]
+
 export const GraphContext = createContext({} as GraphContextInterface);
 
 function App() {
@@ -33,6 +36,8 @@ function App() {
   const { instance, accounts } = useMsal();
   const [graphData, setGraphData] = useState({} as GraphContextInterface);
   const [isLoading, setIsLoading] = useState(true);
+  const [test, setTest] = useState({} as any)
+  const [isUnprivileged, setIsUnprivileged] = useState(false)
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -62,31 +67,42 @@ function App() {
   }, [isAuthenticated, accounts, instance]);
 
   useEffect(() => {
-    /*
     if (graphData?.user?.mail !== undefined) {
       let groups: string[] = [];
       graphData.groups?.value.forEach((g: any) => {
         groups.push(g.displayName);
       });
-      console.log(groups);
+      console.log(groups)
+      let isOk = false
+      allowedGroups.forEach(element => {
+        if (groups.includes(element)) {
+          isOk = true
+        }
+      });
+      if (!isOk) {
+        setIsUnprivileged(true)
+        return
+      }
+
       const data = {
-        mail: graphData.user.mail,
+        employeeEmail: graphData.user.mail,
         roles: groups,
       };
-      fetch(process.env.REACT_APP_API_SERVER + "/api/user/getUser.php", {
+      fetch(process.env.REACT_APP_API_SERVER + "/api/user/getLogin.php", {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "no-cors", // no-cors, *cors, same-origin
+        mode: "cors", // no-cors, *cors, same-origin
         headers: {
           "Content-Type": "application/json",
           // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: JSON.stringify(data), // body data type must match "Content-Type" header
       })
-        .then((response) => response.json)
-        .then((data) => console.log(data));
+        .then((response) => response.json())
+        .then(data => setTest(data));
     }
-    */
   }, [graphData]);
+
+  
 
   useEffect(() => {
     if (graphData?.user?.mail !== undefined) {
@@ -97,6 +113,11 @@ function App() {
 
   if (!isAuthenticated) return <SignIn />;
 
+  if(isUnprivileged) return (<div className="flex justify-center gap-3 items-center h-full flex-col">
+    <h1>Du har inte tillgång till lunchbokningen</h1>
+    <SignOutButton />
+  </div>);
+
   if (isLoading)
     return (
       <div className="h-screen">
@@ -104,7 +125,7 @@ function App() {
       </div>
     );
 
-  console.log(graphData)  
+  console.log(test)  
   return (
     <BrowserRouter>
       <GraphContext.Provider value={graphData}>
