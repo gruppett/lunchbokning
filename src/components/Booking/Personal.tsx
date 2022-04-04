b161df7015366a9361fc4b65cf1c72253946dec9
October45 <vikto45.t@gmail.com>
2022-04-04 21:22:41 +0300

    Periods fetch

--- src/components/Booking/Personal.tsx
+++ src/components/Booking/Personal.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Spinner from "../Spinner/Spinner";
 
 function PersonalBooking() {
-  //function fillPeriodSelect() {}
+  const [periods, setPeriods] = useState([]);
+  const [periodsLoading, setPeriodsLoading] = useState(true);
+  const [periodsError, setPeriodsError] = useState(false);
+
+  useEffect(() => {
+    const fetchPeriods = async () => {
+      const url = process.env.REACT_APP_API_SERVER + "period/getPeriods.php";
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setPeriods(data);
+        setPeriodsLoading(false);
+      } catch (error: any) {
+        setPeriodsError(error.error);
+      }
+    };
+    fetchPeriods();
+  }, []);
+
+  if (periodsLoading || periodsError) {
+    periodsLoading ? <Spinner /> : <script>alert(periodsError)</script>;
+  }
 
   return (
     <>
@@ -20,7 +42,13 @@ function PersonalBooking() {
         <label htmlFor="period" className="m-1 my-2">
           Period:{" "}
         </label>
-        <select name="period" id="periodSelect" className="m-1 my-2"></select>
+        <select name="period" id="periodSelect" className="m-1 my-2">
+          {periods.map((period: any) => (
+            <option key={period.periodID} value={period.periodID}>
+              {period.periodName}
+            </option>
+          ))}
+        </select>
         <button>{}</button>
       </div>
       <hr />
