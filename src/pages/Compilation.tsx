ba60af4af3cdc2864cbc148b74e76d67c6d2519d
Jann Tötterman <jann.totterman@gmail.com>
2022-04-26 09:02:48 +0300

    rework

--- src/pages/Compilation.tsx
+++ src/pages/Compilation.tsx
@@ -1,22 +1,9 @@
-import React, {Key, useEffect, useState, useRef} from 'react'
+import React, {Key, useEffect, useState, useRef, useContext} from 'react'
 import moment from 'moment'
 import Spinner from '../components/Spinner/Spinner'
-import ReactToPrint from "react-to-print";
+import ReactToPrint from "react-to-print"
+import { UserContext } from '../components/App/App'
 
-const tableGuide = [
-  {
-    title: 'Normal',
-    data: 'normal'
-  },
-  {
-    title: 'Diet',
-    data: 'diet'
-  },
-  {
-    title: 'Total',
-    data: 'total'
-  }
-]
 
 function formatDate(date: Date) {
   const dateFormat = "YYYY-MM-DD"
@@ -24,35 +11,63 @@ function formatDate(date: Date) {
 }
 function Compilation() {
   const [daysData, setDaysData] = useState({} as any)
-  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(false as any)
+  const [loading, setLoading] = useState(true)
   const [dates, setDates] = useState({
     startDate: formatDate(new Date()),
-    endDate: moment(formatDate(new Date())).add(7, 'days').format("YYYY-MM-DD")
+    endDate: moment(formatDate(new Date())).add(6, 'days').format("YYYY-MM-DD")
   })
+  const [servings, setServings] = useState([] as any)
+  const { userData } = useContext(UserContext);
   let printRef = useRef(null)
 
   function handleChange(event: any) {
-    setIsLoading(true)
+    setLoading(true)
     const {name, value} = event.target
     setDates({...dates, [name]: value})
   }
 
   useEffect(() => {
-    fetch(process.env.REACT_APP_API_SERVER + "booking/getDays.php", {
-      method: "POST",
-      body: JSON.stringify(dates),
-      headers: {
-        "Content-Type": "application/json"
+    (async () => {
+      try {
+        setLoading(true)
+        const result = await fetch(process.env.REACT_APP_API_SERVER + "booking/getDays.php", {
+          method: "POST",
+          body: JSON.stringify(dates),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        const data = await result.json()
+        if (!result.ok) {
+          setError(data.error)
+          setLoading(false)
+        } 
+        console.log(data)
+        setDaysData(data)
+        setServings(getUniqueServings(data))
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
       }
-    })
-    .then((response) => response.json())
-    .then(data => {
-      setDaysData(data)
-      setIsLoading(false)
-    })
+    })()
   }, [dates])
 
+  function getUniqueServings (data: any) {
+    const result: string[] = []
+    data.forEach((day: { serving: any[]; }) => {
+      day.serving.forEach(serving => {
+        if (!result.includes(serving.name)) {
+          result.push(serving.name)
+        }
+      });
+    });
+    return result
+  }
+
+
   console.log(daysData)
+
 
   return (
     <>
@@ -73,44 +88,65 @@ function Compilation() {
         bodyClass={"m-4"}
         />
       </div>
-      {isLoading ?
-        <Spinner />
-        :<div ref={printRef} className="grid grid-cols-5 grid-rows-auto gap-2">
-          {daysData.error  ?
-           daysData.error.map((error: string, key: Key) => <p key={key}>{error}</p>)
-           : daysData?.map((i:any, key:Key) => (
-             <table key={key}>
-               <tbody>
-                <tr>
-                  <th>
-                    {i.date}
-                  </th>
-                </tr>
-                {i.serving.map((j:any, key:Key) => (
-                  <>
-                  <tr key={key}>
-                    <th>
-                      {j.name}
-                    </th>
-                  </tr>
-                  {tableGuide.map((k:any, key:Key) => (
-                    <tr key={key}>
-                      <th>
-                        {k.title}
-                      </th>
-                      <td>
-                        {j[k.data]}
-                      </td>
-                    </tr>
-                  ))}
-                  </>
-                ))}
-               </tbody>
-             </table>
-          ))}
+      <div className=""  ref={printRef}>
+        <div className="flex gap-3 flex-col">
+          <div>Hämtat {new Date().toLocaleDateString()} av {userData.employeeEmail}</div>
+          <div className='flex gap-3'>
+            <div>T: Total</div>
+            <div>N: Normal</div>
+            <div>D: Diet</div>
+          </div>
         </div>
-      }
+        {error ? error.map((error: string, key: Key) => 
+          <p className='text-red-500' key={key}>{error}</p>
+        ): 
+        loading ?
+          <Spinner />
+          : <div className="flex flex-wrap items-start -mx-1">
+            {daysData.map((i: any, iKey: Key) =>(
+              <div className="w-full w-1/6 my-1 px-1 min-w-min" key={iKey}>
+                <table className="text-left border-collapse border-4 ">
+                  <tbody>
+                    <tr className='border-b-2'>
+                      <th colSpan={2} className="border p-1 text-lg">{i.date}</th>
+                      <th className="border p-1">T</th>
+                      <th className="border p-1">N</th>
+                      <th className="border p-1">D</th>
+                    </tr>
+                    {i.serving.map((j:any, jKey: Key) => {
+                      return j.groups.map((k:any, kKey: Key) => (
+                        (<tr key={kKey} className="even:bg-slate-100">
+                          {kKey === 0 ? <th className="border p-1 bg-white" rowSpan={j.groups.length}>{j.name}</th> : <></>}
+                          <td className="border p-1">{k.name}</td>
+                          <td className="border p-1">{k.total}</td>
+                          <td className="border p-1">{k.normal}</td>
+                          <td className="border p-1">{k.diet}</td>
+                        </tr>)
+                        ))
+                      })}
+                      {i.serving.map((j:any, jKey: Key) => (
+                        <tr key={jKey} className={jKey === 0 ? "border-t-2 even:bg-slate-100" : "even:bg-slate-100"}>
+                          {jKey === 0 ? <th rowSpan={i.serving.length} className="border p-1 bg-white">Subtotal</th> : <></>}
+                          <th className="border p-1">{j.name}</th>
+                          <td className="border p-1">{j.total}</td>
+                          <td className="border p-1">{j.normal}</td>
+                          <td className="border p-1"  >{j.diet}</td>
+                        </tr>
+                      ))}
+                      <tr className='border-t-2'>
+                        <th colSpan={2} className="border p-1">Totalt</th>
+                        <td className="border p-1">{i.total}</td>
+                        <td className="border p-1">{i.normal}</td>
+                        <td className="border p-1">{i.diet}</td>
+                      </tr>
+                  </tbody>
+                </table>
+                </div>
+            ))}
+          </div>
+        }
       </div>
+    </div>
     </>
   )
 }
