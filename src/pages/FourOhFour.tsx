c8b77e71654f6d5147a48ed2e9294b671dd48234
Jann Tötterman <jann.totterman@gmail.com>
2022-04-29 13:13:49 +0300

    404

--- src/pages/FourOhFour.tsx
+++ src/pages/FourOhFour.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function FourOhFour() {
   return (
-    <div>
-      <p>FourOhFour</p>
+    <div className="flex flex-col gap-3 items-center mt-12">
+      <h2 className="text-2xl">404</h2>
+      <h2 className="text-lg text-center">
+        Den här sidan finns tyvärr inte,<br />
+        eller så har du inte tillgång till den.
+      </h2>
+      <Link to="/" className="p-3 bg-green-600 text-white">Tillbaka till startsidan</Link>
     </div>
   );
 }
