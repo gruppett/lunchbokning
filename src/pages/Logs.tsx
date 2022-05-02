324a089b0a1d824e4041049480705b0406ca45da
Jann Tötterman <jann.totterman@gmail.com>
2022-05-02 15:02:58 +0300

    unused import

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState} from "react";
 import Spinner from "../components/Spinner/Spinner";
 import DataTable, {
   TableColumn,
