e4e602469d654d0d739ae73e8f3cad77d4eb5eee
October45 <vikto45.t@gmail.com>
2022-05-05 09:14:51 +0300

    Groups.tsx

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import HjortenCalendar from "../components/Calendar/calendar";
+import GroupBooking from "../components/Booking/Group";
 
 function Groups() {
+  const [booking, setBooking] = useState(null as any);
+  const [bookings, setBookings] = useState(null as any);
+  const [group, setGroup] = useState(null as any);
+
   return (
-    <div>
-      <p>Groups</p>
-    </div>
+    <>
+      <GroupBooking
+        setBooking={setBooking}
+        bookings={bookings}
+        setGroup={setGroup}
+      />
+      <HjortenCalendar
+        view="Groups"
+        group={group}
+        bookingID={booking}
+        setBookings={setBookings}
+      />
+    </>
   );
 }
 
